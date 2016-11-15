45fe4074d32f97b2fef580600ab5e30b05f168cd
pmay5 <paulmay3425@gmail.com>
2016-11-15 10:18:13 -1000

    Team 11 Names
    Changed the team name

--- team11.py
+++ team11.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Paul May and John Kennedy Domingo' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
